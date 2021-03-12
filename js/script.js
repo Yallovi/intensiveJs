c41c8b1909726685e939e94673aa61d8b1d72669
Yallovi <vivimoskalenko@yandex.ru>
2021-03-12 23:49:41 +0300

    Wrote the second option js tabs

--- js/script.js
+++ js/script.js
@@ -4,26 +4,49 @@ document.addEventListener('DOMContentLoaded', () =>{
    const tabs = () =>{
 
        const cardDetailChangeElems = document.querySelectorAll('.card-detail__change');
-       const cardImageElems = document.querySelectorAll('.card__image');
-       const cardDetailsTitleElems = document.querySelectorAll('.card-details__title');
-       
-       const hideAll = () =>{
-        for (let i = 0; i < cardDetailChangeElems.length; i++) {
-            cardDetailChangeElems[i].classList.remove('active');
-            cardImageElems[i].classList.remove('active');
-            cardDetailsTitleElems[i].classList.remove('active');  
-        }
-       };
+       const cardImageElem = document.querySelector('.card__image_item');
+       const cardDetailsTitleElem = document.querySelector('.card-details__title');
+       const priceElem = document.querySelector('.card-details__price');
+       const descriprionMemory = document.querySelector('.description__memory');
 
-       for (let i = 0; i < cardDetailChangeElems.length; i++) {
-           cardDetailChangeElems[i].addEventListener('click', () =>{
-               hideAll();
-               cardDetailChangeElems[i].classList.add('active');
-               cardImageElems[i].classList.add('active');
-               cardDetailsTitleElems[i].classList.add('active'); 
+       const data =[
+           {
+            name: 'Смартфон Apple iPhone 12 Pro 128GB Graphite',
+            img: 'img/iPhone-graphite.png',
+            price: 99990 + '₽',
+            memoryROm: 128
+           },
+           {
+            name: 'Смартфон Apple iPhone 12 Pro 256GB Silver',
+            img: 'img/iPhone-silver.png',
+            price: 120990 + '₽',
+            memoryROm: 256
+           },
+           {
+            name: 'Смартфон Apple iPhone 12 Pro 128GB Pacific Blue',
+            img: 'img/iPhone-blue.png',
+            price: 95990 + '₽',
+            memoryROm: 128
+           }
+       ];
+
+       const deActive = () =>{
+           cardDetailChangeElems.forEach(btn => btn.classList.remove('active'));
+       }        
+
+       cardDetailChangeElems.forEach((btn, i) =>{
+           btn.addEventListener('click', () =>{
+                if(!btn.classList.contains('active')){
+                    deActive();
+                    btn.classList.add('active');
+                    cardDetailsTitleElem.textContent = data[i].name;
+                    cardImageElem.src = data[i].img;
+                    priceElem.textContent = data[i].price;
+                    descriprionMemory.textContent = `>Встроенная память (ROM) ${data[i].memoryROm} ГБ `;
+                }
            });
-           
-       }
+       });
+    
 
 
    };
